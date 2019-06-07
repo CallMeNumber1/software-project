78051db0516c18eda1239c4c7a8d51e4fed4abf6
zhanyeye <zhanyeye@qq.com>
2019-06-07 23:50:54 +0800

    创建 实体类
    创建 Teacher, Exam, TeacherExam 实体类

---
+++ src/main/java/com/example/softwareproject/entity/TeacherExam.java
@@ -0,0 +1,41 @@
+package com.example.softwareproject.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+//创建联合约束
+@Table(name = "TeacherExam", uniqueConstraints = {@UniqueConstraint(columnNames={"teacher_id", "exam_id"})})
+public class TeacherExam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @ManyToOne
+    private Teacher teacher;
+    @ManyToOne
+    private Exam exam;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", updatable = false, insertable = false)
+    private LocalDateTime insertTime;
+
+
+}
