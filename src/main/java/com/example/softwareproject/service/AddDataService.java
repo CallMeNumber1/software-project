4f6cd705bb311781951cd933928a9d033619824b
zhanyeye <zhanyeye@qq.com>
2019-06-16 13:38:53 +0800

    修正初始化数据库数据逻辑，对初始用户胡密码进行加密
     对初始用户胡密码进行加密

--- src/main/java/com/example/softwareproject/service/AddDataService.java
+++ src/main/java/com/example/softwareproject/service/AddDataService.java
@@ -3,6 +3,7 @@ package com.example.softwareproject.service;
 import com.example.softwareproject.entity.Exam;
 import com.example.softwareproject.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,12 +23,8 @@ public class AddDataService {
     private ExamService examService;
     @Autowired
     private UserService userService;
-
-
-    public void init_Admin() {
-        User admin = new User("black","1020","123456",User.ADMIN_AUTHORITY);
-        userService.addUser(admin);
-    }
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public void init_Exam() {
         Exam e1 = new Exam();
@@ -50,24 +47,27 @@ public class AddDataService {
         e3.setBeginTime(LocalDateTime.of(2019, 6, 6, 10, 0, 0));
         e3.setEndTime(LocalDateTime.of(2019, 6, 6, 11, 0, 0));
         examService.addExam(e3);
-
     }
 
     public void init_Teacher() {
-        User u1 = new User("black","3001","123456",User.USER_AUTHORITY);
-        User u2 = new User("while","3002","123456",User.USER_AUTHORITY);
-        User u3 = new User("belly","3003","123456",User.USER_AUTHORITY);
-        User u4 = new User("monk","3004","123456",User.USER_AUTHORITY);
+        User u1 = new User("black","3001",passwordEncoder.encode("123456"),User.USER_AUTHORITY);
+        User u2 = new User("while","3002",passwordEncoder.encode("123456"),User.USER_AUTHORITY);
+        User u3 = new User("belly","3003",passwordEncoder.encode("123456"),User.USER_AUTHORITY);
+        User u4 = new User("monk","3004",passwordEncoder.encode("123456"),User.USER_AUTHORITY);
         userService.addUser(u1);
         userService.addUser(u2);
         userService.addUser(u3);
         userService.addUser(u4);
     }
 
+    public void init_Admin() {
+        User admin = new User("black","1020",passwordEncoder.encode("123456"),User.ADMIN_AUTHORITY);
+        userService.addUser(admin);
+    }
+
     public void init_ExamDetail() {
-//        examService.setExamDetail(1, 1);
-//        examService.setExamDetail(2, 2);
-//        examService.setExamDetail(3, 3);
+        int[] array = new int[]{1, 2, 3};
+        examService.setExamDetail(1,array);
     }
 
 }
